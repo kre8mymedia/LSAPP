7890355ed60098c38aca5f214e48766b7dda3a1d
unknown <kre8mymedia@gmail.com>
2019-07-03 00:07:34 -0500

    Traversy Part 4 - 14:20

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1>{{$title}}</h1>
+  @if(count($services) > 0)
+    @foreach($services as $service)
+      <ul>
+        <li>{{$service}}</li>
+      </ul>
+    @endforeach
+  @endif
+@endsection
